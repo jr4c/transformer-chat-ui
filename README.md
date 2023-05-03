47ab7ea97d55d0cd54b0f50e30fa59a4839fbdbd
Ubuntu <ubuntu@ip-172-31-95-37.ec2.internal>
2023-05-03 16:41:39 +0000

    [code] Implementationof nvidia and Pytthon - Need modification of the installer

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Repository for Manage Chat with Transformers
 
 ### Reference
 Base on Stablelm Tuned Alpha Chat
+
+```
+docker run --gpus all -v ~/cache:/root/.cache/  --name uix --rm -it -p 9603:7860 ui
+```
--- app.py
+++ app.py
@@ -10,12 +10,18 @@ from threading import Thread
 
 print(f"Starting to load the model to memory")
 
-m = AutoModelForCausalLM.from_pretrained(
-    "stabilityai/stablelm-tuned-alpha-7b",
-    torch_dtype=torch.float16
-).cuda()
 
-tok = AutoTokenizer.from_pretrained(
+USER_TOKEN = '<|USER|>'
+ASSISTANT_TOKEN = '<|ASSISTANT|>'
+
+language_model = AutoModelForCausalLM.from_pretrained(
+    "stabilityai/stablelm-tuned-alpha-7b",
+    load_in_8bit=True,
+    device_map='auto',
+    torch_dtype=torch.float16,
+)
+
+tokenizer = AutoTokenizer.from_pretrained(
     "stabilityai/stablelm-tuned-alpha-7b"
 )
 
@@ -31,7 +37,7 @@ start_message = """<|SYSTEM|># StableAssistant
 
 class StopOnTokens(StoppingCriteria):
     def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
-        stop_ids = [50278, 50279, 50277, 1, 0]
+        stop_ids = [50278, 50279, 50277, 1, 0] # TODO: Verify what tokens are this
         for stop_id in stop_ids:
             if input_ids[0][-1] == stop_id:
                 return True
@@ -48,16 +54,15 @@ def chat(curr_system_message, history):
 
     # Construct the input message string for the model by concatenating the current system message and conversation history
     messages = curr_system_message + \
-        "".join(["".join(["<|USER|>"+item[0], "<|ASSISTANT|>"+item[1]])
-                for item in history])
+        "".join(["".join([USER_TOKEN+item[0], ASSISTANT_TOKEN+item[1]]) for item in history])
 
     # Tokenize the messages string
-    model_inputs = tok([messages], return_tensors="pt").to("cuda")
-    streamer = TextIteratorStreamer(tok, timeout=10., skip_prompt=True, skip_special_tokens=True)
+    model_inputs = tokenizer([messages], return_tensors="pt").to("cuda")
+    streamer = TextIteratorStreamer(tokenizer, timeout=30., skip_prompt=True, skip_special_tokens=True)
     generate_kwargs = dict(
         model_inputs,
         streamer=streamer,
-        max_new_tokens=1024,
+        max_new_tokens=1040,
         do_sample=True,
         top_p=0.95,
         top_k=1000,
@@ -65,7 +70,7 @@ def chat(curr_system_message, history):
         num_beams=1,
         stopping_criteria=StoppingCriteriaList([stop])
     )
-    t = Thread(target=m.generate, kwargs=generate_kwargs)
+    t = Thread(target=language_model.generate, kwargs=generate_kwargs)
     t.start()
 
     # print(history)
@@ -83,26 +88,46 @@ def chat(curr_system_message, history):
 with gr.Blocks() as demo:
     # history = gr.State([])
     gr.Markdown("## StableLM-Tuned-Alpha-7b Chat")
-    chatbot = gr.Chatbot().style(height=500)
+    chatbot = gr.Chatbot().style(height=700)
     with gr.Row():
         with gr.Column():
-            msg = gr.Textbox(label="Chat Message Box", placeholder="Chat Message Box",
-                             show_label=False).style(container=False)
+            
+            msg = gr.Textbox(
+                label="Chat Message Box",
+                placeholder="Chat Message Box",
+                show_label=False
+            ).style(container=False)
+
         with gr.Column():
             with gr.Row():
                 submit = gr.Button("Submit")
                 stop = gr.Button("Stop")
                 clear = gr.Button("Clear")
+    
     system_msg = gr.Textbox(
-        start_message, label="System Message", interactive=False, visible=False)
+        start_message,
+        label="System Message",
+        interactive=False,
+        visible=True
+    )
 
-    submit_event = msg.submit(fn=user, inputs=[msg, chatbot], outputs=[msg, chatbot], queue=False).then(
-        fn=chat, inputs=[system_msg, chatbot], outputs=[chatbot], queue=True)
-    submit_click_event = submit.click(fn=user, inputs=[msg, chatbot], outputs=[msg, chatbot], queue=False).then(
-        fn=chat, inputs=[system_msg, chatbot], outputs=[chatbot], queue=True)
-    stop.click(fn=None, inputs=None, outputs=None, cancels=[
-               submit_event, submit_click_event], queue=False)
+    submit_event = msg.submit(
+        fn=user, inputs=[msg, chatbot], outputs=[msg, chatbot], queue=False
+    ).then(
+        fn=chat, inputs=[system_msg, chatbot], outputs=[chatbot], queue=True
+    )
+
+    submit_click_event = submit.click(
+        fn=user, inputs=[msg, chatbot], outputs=[msg, chatbot], queue=False
+    ).then(
+        fn=chat, inputs=[system_msg, chatbot], outputs=[chatbot], queue=True
+    )
+    
+    stop.click(
+        fn=None, inputs=None, outputs=None, cancels=[submit_event, submit_click_event], queue=False
+    )
+    
     clear.click(lambda: None, None, [chatbot], queue=False)
 
 demo.queue(max_size=32, concurrency_count=6)
-demo.launch()
+demo.launch(server_name="0.0.0.0")
